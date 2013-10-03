88a38336faf8e64a6b697e38a7115cae5c1a450a
JAIMB <Aime.TinyPgm@hotmail.com>
2013-10-03 16:18:37 +0700

    add InventoryAddMoreItemActivity to AndroidManifest.xml
    add Intent goAddMoreProduct from inventory_additem to inventory_main

--- MobilePOS/src/com/example/mobilepos/MainActivity.java
+++ MobilePOS/src/com/example/mobilepos/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.mobilepos;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -11,6 +14,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inventory_main);
+        
+       addButton = (Button)findViewById(R.id.inventory_b_additem);
+       addButton.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent goAddMoreProduct = new Intent(getApplicationContext(),InventoryAddMoreItemActivity.class);
+			startActivity(goAddMoreProduct);
+		}
+	});
+        
+        
     }
 
 

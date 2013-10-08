fab185ea1809ee17bc81590af3514adc670528fa
AimeTPGM <Aime.TinyPgm@hotmail.com>
2013-10-08 22:38:55 +0700

    update AndroidManifest.xml
    edit SaleSelectItemActivity.java

--- MobilePOS/src/mobilepos/activity/SaleSelectItemActivity.java
+++ MobilePOS/src/mobilepos/activity/SaleSelectItemActivity.java
@@ -1,7 +1,17 @@
 package mobilepos.activity;
 
+import com.example.mobilepos.R;
+
 import android.app.Activity;
+import android.os.Bundle;
 
 public class SaleSelectItemActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sale_item_in_inventory);
+	}
 
 }

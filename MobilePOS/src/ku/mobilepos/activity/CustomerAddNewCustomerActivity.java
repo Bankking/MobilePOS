45079106f345a10b7d52e1d60ddf35ab0e8a6683
JAIMB <Aime.TinyPgm@hotmail.com>
2013-11-20 15:32:36 +0700

    add CustomerAddNewCustomerActivity.java

---
+++ MobilePOS/src/ku/mobilepos/activity/CustomerAddNewCustomerActivity.java
@@ -0,0 +1,18 @@
+package ku.mobilepos.activity;
+
+import com.example.mobilepos.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CustomerAddNewCustomerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.customer_addnewcus);
+		
+	}
+
+}
